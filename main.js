867392bb89db519524ec441f05d436a5f5c26bac
elbeknurov <elbeknurov11@gmail.com>
2022-12-19 14:48:36 +0500

    film

---
+++ main.js
@@ -0,0 +1,29 @@
+const template = document.querySelector("#template");
+const elDiv = document.querySelector(".card");
+
+let film = [];
+fetch(`https://www.omdbapi.com/?s=avengers&plot=full&apikey=e2f9a30`)
+  .then((res) => res.json())
+  .then((data) => {
+    film = data.Search;
+    getFunction(film);
+  });
+
+function getFunction(query) {
+  const fragment = document.createDocumentFragment();
+
+  query.forEach((film) => {
+    const constTemplate = template.content.cloneNode(true);
+    const img = constTemplate.querySelector(".poster");
+    const title = constTemplate.querySelector(".title");
+    const year = constTemplate.querySelector(".year");
+    const type = constTemplate.querySelector(".series");
+
+    img.src = film.Poster;
+    title.textContent = film.title;
+    year.textContent = film.year;
+    type.textContent = film.Type;
+    fragment.appendChild(constTemplate);
+  });
+  elDiv.appendChild(fragment);
+}
